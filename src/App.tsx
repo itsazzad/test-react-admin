a3c30385c2b0db2b30412cc2922ff86126a51b29
Sazzad Hossain Khan <itsazzad@gmail.com>
2023-08-29 11:41:35 +0600

    none

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
-import { dataProvider } from './dataProvider';
 
 export const App = () => (
     <Admin
-        dataProvider={dataProvider}
-	>
+        >
         <Resource name="XXX" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
     </Admin>
 );
